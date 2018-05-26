679085f2cbb972fe28d2ea349fa85f801aba2ce9
kbseah <kbseah@mpi-bremen.de>
2018-05-26 20:23:26 +0200

    Add new plot scripts

--- plot_changestats.R
+++ plot_changestats.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 currencies <- c('EUR','USD','JPY','GBP')
 
-#Amt Comb Avg Bestcount_count Bestcount_number Bestcount_wt Bestwt Bestwt_count Bestwt_number Curr
+#Amt Comb Avg Bestcount_count Bestcount_number Bestcount_wt Bestwt Bestwt_count Bestwt_number Curr Greedy_count
 changestats <- data.frame()
 for (i in 1:length(currencies)) {
   d <- read.table(paste(c("changestats/changestats_100_",currencies[i],".tsv"),sep="",collapse=""),header=TRUE,sep="\t")
@@ -14,14 +14,25 @@ for (i in 1:length(currencies)) {
   changestats <- rbind(changestats,dd)
 }
 
-png("currency_comparison_changestats_counts.png",height=300,width=1200)
-ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Bestwt_count, color="lowest weight count"))  + geom_line(aes(y=Bestcount_count, color="fewest coins")) + facet_wrap(~Curr,nrow=1) + labs(x='Amount to change (¢)',y='Number of coins')
+png("currency_comparison_changestats_optimal_counts.png",height=600,width=1200)
+ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Bestcount_count, color="fewest coins")) + facet_wrap(~Curr,nrow=2) + labs(x='Amount to change (¢)',y='Number of coins')
 dev.off()
 
-png("currency_comparison_changestats_weights.png",height=300,width=1200)
-ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Bestwt, color="lowest weight")) + geom_line(aes(y=Bestcount_wt, color="fewest coin weight")) + facet_wrap(~Curr,nrow=1) + labs(x='Amount to change (¢)',y='Weight (g)')
+png("currency_comparison_changestats_counts.png",height=600,width=1200)
+ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Bestwt_count, color="lowest weight count"))  + geom_line(aes(y=Bestcount_count, color="fewest coins")) + facet_wrap(~Curr,nrow=2) + labs(x='Amount to change (¢)',y='Number of coins')
 dev.off()
 
-png("currency_comparison_changestats_greedy.png",height=300,width=1200)
-ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Greedy_count, color="greedy count"))  + geom_line(aes(y=Bestcount_count, color="fewest coins")) + facet_wrap(~Curr,nrow=1) + labs(x='Amount to change (¢)',y='Number of coins')
+png("currency_comparison_changestats_weights.png",height=600,width=1200)
+ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Bestwt, color="lowest weight")) + geom_line(aes(y=Bestcount_wt, color="fewest coin weight")) + facet_wrap(~Curr,nrow=2) + labs(x='Amount to change (¢)',y='Weight (g)')
+dev.off()
+
+png("currency_comparison_changestats_greedy.png",height=600,width=1200)
+ggplot(changestats,aes(x=Amt)) + geom_line(aes(y=Greedy_count, color="greedy count"))  + geom_line(aes(y=Bestcount_count, color="fewest coins")) + facet_wrap(~Curr,nrow=2) + labs(x='Amount to change (¢)',y='Number of coins')
+dev.off()
+
+# Plot greedy vs optimal for LSD
+d.lsd <- read.table("changestats/changestats_100_LSD_part.tsv",header=TRUE,sep="\t")
+
+png("currency_comparison_changestats_LSD_greedy.png",height=300,width=1200)
+ggplot(d.lsd,aes(x=Amt)) + geom_line(aes(y=Greedy_count, color="greedy count"))  + geom_line(aes(y=Bestcount_count, color="fewest coins")) + labs(x='Amount to change (¢)',y='Number of coins')
 dev.off()
